6552afb41d726b99ea74eb780d00a78f2446f05d
jdi011 <jdi011@jdi011s-Mac-mini.local>
2020-04-07 10:10:16 +0530

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDom from 'react-dom';
+import SeasonDisplay from './seasonDisplay';
+import Spinner from './spinner';
+class App extends React.Component {
+    state = { lat: null, long: null, errorMessage: ''};
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    componentDidMount(){
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => this.setState ({ lat: position.coords.latitude, long: position.coords.longitude }),
+            (err) => this.setState({ errorMessage : err.message })
+        );
+    }
+    
+    renderContent() {
+        if(this.state.errorMessage && !this.state.lat){
+            return(
+            <div>
+                Error: {this.state.errorMessage}
+            </div>
+            );
+        }
+        if(!this.state.errorMessage && this.state.lat){
+            return (
+                <div>
+                    <SeasonDisplay 
+                    lat={this.state.lat}
+                    long={this.state.long}
+                    />
+                </div>
+                );        
+        }
+        return <Spinner message="Please accept location request"/>;
+    };
+
+    render () {
+        return(
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        )
+    }
+}
+
+ReactDom.render(
+    <App />,
+    document.querySelector('#root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
